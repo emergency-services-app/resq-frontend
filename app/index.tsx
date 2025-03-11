c0b4119969e4e06bb982d246e09c998552a71432
Rohit Jung Kathet <rokshh28@gmail.com>
2025-03-11 14:30:15 +0545

    test: testing map integration

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,64 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Platform, Text } from 'react-native';
+import { useEffect, useState } from 'react';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+export default function HomeScreen() {
+	const [location, setLocation] = useState<Location.LocationObject | null>(
+		null
+	);
+	const [errorMsg, setErrorMsg] = useState<String | null>(null);
+
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== 'granted') {
+				setErrorMsg('Permission to access location not granted');
+				return;
+			}
+
+			let location = await Location.getCurrentPositionAsync();
+			setLocation(location);
+		})();
+	}, []);
+
+	return (
+		<>
+			{/* <Text>{errorMsg ? errorMsg : 'Location Loaded'}</Text> */}
+			{location && (
+				<MapView
+					style={styles.map}
+					initialRegion={{
+						latitude: location.coords.latitude,
+						longitude: location.coords.longitude,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421,
+					}}
+				>
+					<Marker
+						coordinate={{
+							latitude: location.coords.latitude,
+							longitude: location.coords.longitude,
+						}}
+						title="You are here"
+						description="This is your current location"
+					/>
+				</MapView>
+			)}
+			<MapView style={styles.map} />
+		</>
+	);
 }
+
+const styles = StyleSheet.create({
+	titleContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 8,
+	},
+	map: {
+		width: '100%',
+		height: '100%',
+	},
+});
