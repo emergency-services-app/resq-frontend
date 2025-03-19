7d6e931dff40d4b8ac31b7302c8fe814ca552e52
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-03-19 21:09:25 +0545

    feat: implement authentication redirection in home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,66 +1,25 @@
-import { Image, StyleSheet, Platform, Text } from 'react-native';
-import { useEffect, useState } from 'react';
+import { useAuth } from "@/context/authContext";
+import { Redirect, Stack } from "expo-router";
+import React from "react";
 
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
+const Index = () => {
+	const { authState } = useAuth();
 
-export default function HomeScreen() {
-	const [location, setLocation] = useState<Location.LocationObject | null>(
-		null
-	);
-	const [errorMsg, setErrorMsg] = useState<String | null>(null);
-
-	useEffect(() => {
-		(async () => {
-			let { status } = await Location.requestForegroundPermissionsAsync();
-			if (status !== 'granted') {
-				setErrorMsg('Permission to access location not granted');
-				return;
-			}
-
-			let location = await Location.getCurrentPositionAsync();
-			setLocation(location);
-		})();
-	}, []);
+	if (!authState?.authenticated) {
+		return (
+			<Redirect
+				href="/sign-in"
+				relativeToDirectory={false}
+			/>
+		);
+	}
 
 	return (
-		<>
-			<Text className="text-red-400 font-thin">
-				{errorMsg ? errorMsg : 'Location Loaded'}
-			</Text>
-			{location && (
-				<MapView
-					style={styles.map}
-					initialRegion={{
-						latitude: location.coords.latitude,
-						longitude: location.coords.longitude,
-						latitudeDelta: 0.0922,
-						longitudeDelta: 0.0421,
-					}}
-				>
-					<Marker
-						coordinate={{
-							latitude: location.coords.latitude,
-							longitude: location.coords.longitude,
-						}}
-						title="You are here"
-						description="This is your current location"
-					/>
-				</MapView>
-			)}
-			<MapView style={styles.map} />
-		</>
+		<Redirect
+			href="/(tabs)/home"
+			relativeToDirectory={false}
+		/>
 	);
-}
+};
 
-const styles = StyleSheet.create({
-	titleContainer: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		gap: 8,
-	},
-	map: {
-		width: '100%',
-		height: '100%',
-	},
-});
+export default Index;
