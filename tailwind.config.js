cc8ad63103814224916088a69a6ead4fa8608545
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-04-08 14:54:57 +0545

    feat: update config for components and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	// NOTE: Update this to include the paths to all of your component files.
-	content: ['./app/**/*.{js,jsx,ts,tsx}'],
-	presets: [require('nativewind/preset')],
+	content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+	presets: [require("nativewind/preset")],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				Laila: ["Laila-Regular", "sans-serif"],
+				LailaLight: ["Laila-Light", "sans-serif"],
+				LailaMedium: ["Laila-Medium", "sans-serif"],
+				LailaBold: ["Laila-Bold", "sans-serif"],
+				LailaSemiBold: ["Laila-SemiBold", "sans-serif"],
+			},
+			colors: {},
+		},
 	},
 	plugins: [],
 };
